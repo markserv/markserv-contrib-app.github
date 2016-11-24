a7a866961759a5967a2c31305dc969a239bb1ee5
F1LT3R <al@pwn.io>
2016-11-24 02:42:33 -0500

    Staying inline w/ cli

--- Markconf.js
+++ Markconf.js
@@ -1,19 +1,20 @@
 const Markconf = {
-  // includers: {
-  //   html: 'markserv-contrib-inc.html',
-  //   markdown: 'markserv-contrib-inc.markdown',
-  //   less: 'markserv-contrib-inc.less'
-  // },
+  includers: {
+    html: 'markserv-contrib-inc.html',
+  //   // markdown: 'markserv-contrib-inc.markdown',
+  //   // less: 'markserv-contrib-inc.less'
+  },
 
   modifiers: {
-    // '**/': [
-    //   {'markserv-contrib-mod.dir': 'partials/directory.html'},
-    //   {'markserv-contrib-mod.foo': 'partials/directory.html'}
-    // ],
-    '**/': 'markserv-contrib-mod.dir',
-    // '**/*.md': 'markserv-contrib-mod.markdown',
+    '**/': [
+      // 'markserv-contrib-mod.dir',
+      {'markserv-contrib-mod.dir': 'partials/directory.html'},
+    // // //   // {'markserv-contrib-mod.foo': 'partials/directory.html'}
+    ],
+    // '**/': 'markserv-contrib-mod.dir',
+    '**/*.md': 'markserv-contrib-mod.markdown',
     '**/*': 'markserv-contrib-mod.file',
-    // 404: 'markserv-contrib-mod.http-404'
+    404: 'markserv-contrib-mod.http-404'
   }
 };
 
