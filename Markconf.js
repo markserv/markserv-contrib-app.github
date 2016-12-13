c111715307c9bd1860ea7839634a8ed4a6c4c0d6
F1LT3R <al@pwn.io>
2016-12-13 10:15:05 -0500

    Add html modifier and remove old api ver of arry obj config.

--- Markconf.js
+++ Markconf.js
@@ -1,22 +1,18 @@
 const Markconf = {
-  includers: {
-    html: 'markserv-contrib-inc.html',
-    less: 'markserv-contrib-inc.less',
-    markdown: 'markserv-contrib-inc.markdown'
-    // svg: 'markserv-contrib-inc.svg',
-  },
+	includers: {
+		html: 'markserv-contrib-inc.html',
+		less: 'markserv-contrib-inc.less',
+		markdown: 'markserv-contrib-inc.markdown'
+	// svg: 'markserv-contrib-inc.svg',
+	},
 
-  modifiers: {
-    '**/': [
-    // //   // 'markserv-contrib-mod.dir',
-      {'markserv-contrib-mod.dir': 'partials/directory.html'}
-    // // // // //   // {'markserv-contrib-mod.foo': 'partials/directory.html'}
-    ],
-    // '**/': 'markserv-contrib-mod.dir',
-    '**/*.md': 'markserv-contrib-mod.markdown',
-    '**/*': 'markserv-contrib-mod.file',
-    404: 'markserv-contrib-mod.http-404'
-  }
+	modifiers: {
+		'**/': 'markserv-contrib-mod.dir',
+		'**/*.md': 'markserv-contrib-mod.markdown',
+		'**/*.html': 'markserv-contrib-mod.file',
+		'**/*': 'markserv-contrib-mod.file',
+		404: 'markserv-contrib-mod.http-404'
+	}
 };
 
 module.exports = Markconf;
